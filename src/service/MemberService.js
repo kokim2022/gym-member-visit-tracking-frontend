89b747c3e785793dc8ce9e2f0fc392e31a850dec
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-07-13 15:28:53 +0700

    fixed api url

--- src/service/MemberService.js
+++ src/service/MemberService.js
@@ -1,9 +1,11 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export class MemberService {
     getMembers() {
-        return fetch('http://gym-member-visit-tracking-backend.test/api/members', { headers: { 'Cache-Control': 'no-cache' } }).then((res) => res.json());
+        return fetch(`${apiUrl}/members`, { headers: { 'Cache-Control': 'no-cache' } }).then((res) => res.json());
     }
 
     getMemberVisits() {
-        return fetch('http://gym-member-visit-tracking-backend.test/api/member_visits', { headers: { 'Cache-Control': 'no-cache' } }).then((res) => res.json());
+        return fetch(`${apiUrl}/member_visits`, { headers: { 'Cache-Control': 'no-cache' } }).then((res) => res.json());
     }
 }
