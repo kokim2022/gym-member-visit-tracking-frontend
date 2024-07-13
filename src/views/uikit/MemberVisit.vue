49bf3709a5a660f28ee44da970953d2a8d02cad2
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-07-13 15:07:20 +0700

    member visit text color issue fixed

--- src/views/uikit/MemberVisit.vue
+++ src/views/uikit/MemberVisit.vue
@@ -7,6 +7,17 @@ const customer1 = ref(null);
 const loading1 = ref(null);
 
 const memberService = new MemberService();
+const getHourColor = (date) => {
+    const hour = new Date(date).getHours();
+    const colors = ['#FF5733', '#FFBD33', '#DBFF33', '#75FF33', '#33FF57', '#33FFBD', '#33DBFF', '#3375FF', '#5733FF', '#BD33FF', '#FF33DB', '#FF3375'];
+    const bgColor = colors[hour % colors.length];
+
+    // Calculate brightness to determine text color
+    const brightness = (parseInt(bgColor.substr(1, 2), 16) * 299 + parseInt(bgColor.substr(3, 2), 16) * 587 + parseInt(bgColor.substr(5, 2), 16) * 114) / 1000;
+    const textColor = brightness > 128 ? 'black' : 'white';
+
+    return { backgroundColor: bgColor, color: textColor };
+};
 
 onBeforeMount(() => {
     memberService.getMemberVisits().then((data) => {
@@ -31,7 +42,9 @@ onBeforeMount(() => {
                     </Column>
                     <Column field="Visit Date (Entry Date)" header="Visit Date (Entry Date)" dataType="date" style="min-width: 10rem">
                         <template #body="{ data }">
-                            {{ new Date(data.created_at).toLocaleString() }}
+                            <div :style="getHourColor(data.created_at)" class="p-2">
+                                {{ new Date(data.created_at).toLocaleString() }}
+                            </div>
                         </template>
                     </Column>
                 </DataTable>
